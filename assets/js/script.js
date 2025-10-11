858ccf4c827c15c5549787062eade686c0c6ec95
almdneto <airtinpvp13@gmail.com>
2025-10-11 09:46:40 -0300

    3 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,8 @@ const navLinks = document.getElementById('navbar-links');
 
 menuBtn.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+  menuBtn.classList.toggle('ativo');
+
+  const expanded = menuBtn.getAttribute('aria-expanded') === 'true';
+  menuBtn.setAttribute('aria-expanded', String(!expanded));
 });
