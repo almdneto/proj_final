4669caae37c894b01a935ee9f85a51980992f29c
almdneto <airtinpvp13@gmail.com>
2025-10-20 09:05:44 -0300

    Navbar finalizada / Sobre Nós finalizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const botao = document.querySelector("#hamb-btt");
-const menu = document.querySelector("#navbar-links");
+const botao = document.getElementById("hamb-btt");
+const menu = document.getElementById("navbar-links");
 
 botao.addEventListener('click', (e) => {
   e.stopImmediatePropagation();
@@ -10,6 +10,20 @@ botao.addEventListener('click', (e) => {
 document.addEventListener('click', (e) => {
   if (menu.classList.contains('active') && !menu.contains(e.target) && e.target !== botao) {
     menu.classList.remove('active');
-    botao.classList.remove('open')
+    botao.classList.remove('open'); 
   }
 });
+
+const bttcont = document.getElementById("cont")
+const navcont = document.getElementById("navcont")
+
+bttcont.addEventListener('click', (e) => {
+  e.stopImmediatePropagation();
+navcont.classList.toggle('ativo')
+});
+
+document.addEventListener('click', (e => {
+  if (navcont.classList.contains('ativo') && !menu.contains(e.target) && e.target !== bttcont) {
+    navcont.classList.remove('ativo')
+  }
+}))
