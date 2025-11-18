939de4a74f9e050106348ce166438dbb4706cc61
vinicius591 <agathacunha45@gmail.com>
2025-11-18 14:09:44 -0300

    fazendo a manchete

--- js/script.js
+++ js/script.js
@@ -14,20 +14,17 @@ botao.addEventListener('click', (e) => {
   }
 });
 
-document.addEventListener('click', (e) => {
-  if (menu.classList.contains('active') && !menu.contains(e.target) && e.target !== botao) {
-    menu.classList.remove('active');
-    botao.classList.remove('open'); 
-  }
-});
-
 bttcont.addEventListener('click', (e) => {
   e.stopImmediatePropagation();
 navcont.classList.toggle('ativo')
 });
 
-document.addEventListener('click', (e => {
-  if (navcont.classList.contains('ativo') && !menu.contains(e.target) && e.target !== bttcont) {
-    navcont.classList.remove('ativo')
+document.addEventListener('click', (e) => {
+  if (menu.classList.contains('active') && !menu.contains(e.target) && e.target !== botao) {
+    menu.classList.remove('active');
+    botao.classList.remove('open'); 
   }
-}))
+  if (navcont.classList.contains('ativo') && !menu.contains(e.target) && e.target !== bttcont) {
+  navcont.classList.remove('ativo')
+  }
+});
